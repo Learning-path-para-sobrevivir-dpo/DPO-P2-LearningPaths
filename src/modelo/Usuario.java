b43602f2193ebf6b29a83b7be3f58ca202537338
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-10-19 16:15:18 -0500

    Manejo de usuarios terminado

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -13,6 +13,45 @@ public abstract class Usuario {
 		this.login = login;
 		this.correo = correo;
 		this.contraseña = contraseña;
+		if (tipo.toLowerCase().equals(PROFESOR))
+		{
+			this.tipo = PROFESOR;
+		} else if (tipo.toLowerCase().equals(ESTUDIANTE))
+		{
+			this.tipo = ESTUDIANTE;
+		}
+		
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
 	
