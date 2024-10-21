f7489a3941c7e362514251f5134017b727c624a3
manugalarza <m.galarza@uniandes.edu.co>
2024-10-21 01:30:42 -0500

    falta

--- src/persistencia/PersistenciaUsuarios.java
+++ src/persistencia/PersistenciaUsuarios.java
@@ -2,16 +2,19 @@ package persistencia;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import modelo.Usuario;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 
 public class PersistenciaUsuarios {
-    private static final String FILE_PATH = "ruta/a/la/carpeta/usuarios.json"; //TODO: definir esto
+    private static final String ARCHIVO_USUARIOS = "usuarios.json"; //TODO: definir esto
 
     // Cargar usuarios desde el archivo JSON
     public static HashMap<List<String>, Usuario> cargarUsuarios() {
-        try (Reader reader = new FileReader(FILE_PATH)) {
+        try (Reader reader = new FileReader(ARCHIVO_USUARIOS)) {
             Gson gson = new Gson();
             return gson.fromJson(reader, new TypeToken<HashMap<List<String>, Usuario>>(){}.getType());
         } catch (IOException e) {
@@ -22,7 +25,7 @@ public class PersistenciaUsuarios {
 
     // Guardar usuarios en el archivo JSON
     public static void guardarUsuarios(HashMap<List<String>, Usuario> usuarios) {
-        try (Writer writer = new FileWriter(FILE_PATH)) {
+        try (Writer writer = new FileWriter(ARCHIVO_USUARIOS)) {
             Gson gson = new Gson();
             gson.toJson(usuarios, writer);
         } catch (IOException e) {
