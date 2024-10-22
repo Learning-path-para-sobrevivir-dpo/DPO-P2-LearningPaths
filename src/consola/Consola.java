99ca09aa8d473266825e9ac31b9a4d56afa309a5
Manu_Love <107737902+mloveral@users.noreply.github.com>
2024-10-21 23:35:05 -0500

    Avances consola

--- src/consola/Consola.java
+++ src/consola/Consola.java
@@ -1,9 +1,35 @@
 package consola;
 
+import java.util.Scanner;
+
 import persistencia.ManejoDatos;
 
 public class Consola {
 	public static void main(String[] args) {
 		ManejoDatos datos = new ManejoDatos();
+		Consola consola = new Consola();
+		Scanner scanner = new Scanner(System.in);
+		
+		consola.iniciarAplicacion(datos, scanner);
+		
+		scanner.close();
+	}
+	
+	public Consola() {
+		super();
+	}
+
+	public void iniciarAplicacion(ManejoDatos datos, Scanner scanner)
+	{
+		this.mostrarMenuInicio();
+	}
+
+	public void mostrarMenuInicio()
+	{
+		System.out.println("Bienvenido a ......");
+		System.out.println("Seleccione lo que quiere hacer: ");
+		System.out.println("1. Iniciar Sesión");
+		System.out.println("2. Crear Perfil");
+		System.out.println("3. Salir");
 	}
 }
