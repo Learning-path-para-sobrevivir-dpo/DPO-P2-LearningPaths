99c08ae465879ac763dd24e17acffe36ffdebc98
Sara <s.garcia11234@uniandes.edu.co>
2024-10-23 23:08:31 -0500

    add path0

--- src/consola/Consola.java
+++ src/consola/Consola.java
@@ -66,8 +66,7 @@ public class Consola {
         //Se crea un LP al que la estudiante ya está inscrita
         LearningPath path0 = new LearningPath ("Arte y Sociedad", "Curso creativo de arte", "Aprender sobre arte y sociedad", 
         		1, 0, "10/03/2024", "13/05/2024", 2, profesor1.getLogin());
-        estudiante1.inscribirLearningPath(path0);
-        datos.addLearningPath(path0);
+        estudiante1.inscribirLearningPath(path0);s
         Progreso pEst1LP0 = new Progreso(path0.getTitulo(), estudiante1.getLogin());
         
         //Add actividad a Path0
