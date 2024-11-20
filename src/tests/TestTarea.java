cf4a7d798156004b1687500effec370b25ced856
admin <admin@localhost>
2024-11-19 19:28:36 -0500

    test tarea, recurso, preguntaAbierta

--- src/tests/TestTarea.java
+++ src/tests/TestTarea.java
@@ -1,5 +1,6 @@
 package tests;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,18 +12,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import modelo.LearningPath;
-import modelo.Progreso;
 import modelo.Review;
-import modelo.actividades.Examen;
-import modelo.actividades.QuizOpcionMultiple;
-import modelo.actividades.QuizVerdaderoFalso;
 import modelo.actividades.RecursoEducativo;
 import modelo.actividades.Tarea;
 
-public class TestRecursoEducativo {
 
-	private RecursoEducativo recurso;
+public class TestTarea {
+
+	private Tarea tarea;
 	private Review rev1;
 	private Review rev2;
 	
@@ -31,7 +28,7 @@ public class TestRecursoEducativo {
 	@BeforeEach
 	public void setUp() {
 		
-		recurso = new RecursoEducativo ("Video-ensayo de una película", "Analizar pelicula", 1, 15, true,  360, "Recurso Educativo", "Video", "Análisis de una película", "https://www.youtube.com/watch?v=VvclV0_o0JE");
+		tarea = new Tarea ("Tarea", "Practicar conocimientos", 1, 30, true,  360, "Tarea",  "Hacer 5 ejercicios.");
 		rev1 = new Review("2023-10-11", "Me encantó el video", "Reseña", 5);
 	
 		rev2 = new Review("2023-9-10", "Muy interesante", "Reseña", 4);
@@ -42,7 +39,7 @@ public class TestRecursoEducativo {
 	@AfterEach
 	public void tearDown()
 	{
-		recurso = null;
+		tarea = null;
 
 		rev1= null;
 		rev2 = null;
@@ -53,13 +50,15 @@ public class TestRecursoEducativo {
 	@Test
 	public void testCompletarDescompletarActividad() {
 		
-		recurso.completarActividad();
+		tarea.setEnviado(true);
+		boolean completada = tarea.completarActividad();
 		
-		assertEquals("Exitosa", recurso.getEstado(), "No se marcó completada correctamente.");
+		assertEquals(true, completada, "No se marcó completada correctamente.");
 		
-		recurso.descompletarActividad();
+		tarea.setEstado("No Exitosa");
+		tarea.descompletarActividad();
 		
-		assertEquals("", recurso.getEstado(), "No se marcó descompletada correctamente.");
+		assertEquals(false, tarea.isEnviado(), "No se marcó descompletada correctamente.");
 
 		
 	}
@@ -73,15 +72,14 @@ public class TestRecursoEducativo {
 		revs.add(rev1);
 		revs.add(rev2);
 		
-		recurso.setReviews(revs);
+		tarea.setReviews(revs);
 		
-		assertEquals(2, recurso.getReviews().size(), "No tiene las reviews esperadas");
-		assertEquals(4.5, recurso.getRatingPromedio(), "No tiene el promedio esperado.");
+		assertEquals(2, tarea.getReviews().size(), "No tiene las reviews esperadas");
+		assertEquals(4.5, tarea.getRatingPromedio(), "No tiene el promedio esperado.");
 		
 		
 		
 	}
 	
-
 	
 }
